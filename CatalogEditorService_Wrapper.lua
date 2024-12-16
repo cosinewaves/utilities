6706387a5dba41f99997e7518247537167a6cd58
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2024-12-16 18:57:29 +0000

    Create CatalogEditorService_Wrapper.lua

---
+++ CatalogEditorService_Wrapper.lua
@@ -0,0 +1,21 @@
+--[[
+
+	Name: CatalogEditorService_Wrapper.lua
+	Date: 16/12/24
+	Author: cosinewaves
+	
+	Description: Wrapper for CatalogEditorService.SearchCatalog()
+	Documentation: https://developer.roblox.com/en-us/api-reference/function/CatalogEditorService/SearchCatalog
+
+]]
+
+return function (CIS_PARAMS: (CatalogSearchParams | "Default")?): ({any})?
+	return game:GetService("AvatarEditorService"):SearchCatalog(CatalogSearchParams.new({
+		AssetTypes = CIS_PARAMS and CIS_PARAMS.AssetTypes or {Enum.AvatarAssetType.Hat},
+		CreatorName = CIS_PARAMS and CIS_PARAMS.CreatorName or "ROBLOX",
+		IncludeOffSale = CIS_PARAMS and CIS_PARAMS.IncludeOffSale or false,
+		SortType = CIS_PARAMS and CIS_PARAMS.SortType or Enum.CatalogSortType:GetEnumItems()[2],
+		SalesTypeFilter = CIS_PARAMS and CIS_PARAMS.SalesTypeFilter or Enum.SalesTypeFilter:GetEnumItems()[2],
+		Limit = CIS_PARAMS and CIS_PARAMS.Limit or 60
+	})):GetCurrentPage() or nil
+end
